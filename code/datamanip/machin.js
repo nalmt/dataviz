63efdfebfb519a84a2c90ad004a614804c68bc9e
emnabarred <barredemna@gmail.com>
2021-01-13 05:47:26 +0100

    added credits

---
+++ code/datamanip/machin.js
@@ -0,0 +1,4 @@
+for (let i = 1961; i < 2017;i++) {
+    console.log("if (x=="+i+") drawTreemap(\"datasets/EFcons/"+i+".json\");");
+   // console.log("<\option value=" + i + ">" + i + "<\/option>");
+}
