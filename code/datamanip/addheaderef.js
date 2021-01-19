415d09037361563e7509d3724f5e9a9456ee18c8
emnabarred <barredemna@gmail.com>
2021-01-19 18:16:07 +0100

    final edit on main code

---
+++ code/datamanip/addheaderef.js
@@ -0,0 +1,20 @@
+const fs=require('fs');
+
+for (let pas = 1961; pas < 2017; pas++) {
+
+    var obj = JSON.parse(fs.readFileSync(pas+'.json', 'utf8'));
+    //result = "";
+
+
+
+    result = "{\"key\": \"Ecological footprint (GHA) "+pas+"\",\n" +
+        "  \"children\":" + JSON.stringify(obj) + "}";
+   // console.log(result)
+
+    fs.appendFile("data/"+pas+'.json', JSON.stringify(JSON.parse(result)), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+
+
---
+++ code/datamanip/jsonformattingefall.js
@@ -0,0 +1,35 @@
+const fs=require('fs');
+
+for (let pas = 1961; pas < 2017; pas++) {
+
+    var obj = JSON.parse(fs.readFileSync('data/ef/'+pas+'.json', 'utf8'));
+   // result = "";
+
+    result = obj.reduce((r, { nomcontinent: key, ...object }) => {
+        var temp = r.find(o => o.key === key);
+        if (!temp) r.push(temp = { key, children: [] });
+        temp.children.push(
+            {"key" : object.GFN_name,
+                "value":object.total,
+                "children":
+
+                                [
+                                    {"key": "carbon" , "value":object.carbon},
+                                    {"key": "crop land" , "value":object.crop_land},
+                                    {"key": "fishing ground" , "value":object.fishing_ground},
+                                    {"key": "forest land" , "value":object.forest_land},
+                                    {"key": "grazing land" , "value":object.grazing_land},
+                                    {"key": "builtup land" , "value":object.built_up_land}
+                                ]
+
+            });
+        return r;
+    }, []);
+
+   // result = "{\n" + "  \"key\": \"Biocapacity (GHA) "+pas+"\"," + result + "}";
+
+    fs.appendFile(pas+'.json', JSON.stringify(result), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
